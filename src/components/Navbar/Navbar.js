1b432ea9a0ea5386137530bab03bfa022422535d
schmidgallm <schmidgallm@outlook.com>
2020-01-04 22:45:25 -0700

    init commit, index.html boilerplate start, basic navbar, auth components, utils folder

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='nav-wrapper'>
+        <Link to='/' className='brand-logo'>
+          Dreamers
+        </Link>
+        <ul id='nav-mobile' className='right hide-on-med-and-down'>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/register'>Register</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
