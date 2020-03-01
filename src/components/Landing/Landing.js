c8099c9a1c6a3ac9d48c67d9295fd1854c243906
schmidgallm <schmidgallm@outlook.com>
2020-03-01 14:20:16 -0700

    init landing page header

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import mediumImg from './img/header-img-medium.jpg';
 import './Landing.css';
 
 const Landing = () => {
-  return <div>Landing Page</div>;
+  return (
+    <Fragment>
+      <div className='header'>
+        <div className='header-text'>
+          <h1>
+            Create. Share. <span>Inspire</span>
+          </h1>
+          <p>For authors by authors. Inspire and be inspired.</p>
+          <button className='btn btn-primary'>Begin</button>
+        </div>
+        <div className='header-img'>
+          <img src={mediumImg} alt='inspirational' />
+        </div>
+      </div>
+      <div className='feature'>dkfjkdfj</div>
+    </Fragment>
+  );
 };
 
 export default Landing;
