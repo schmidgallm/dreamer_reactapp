278011203f1668c32952554e102f6ff931bc6a2e
schmidgallm <schmidgallm@outlook.com>
2020-08-08 22:01:12 -0700

    prompt page ui begin

--- src/components/Pages/Prompt/Prompt.js
+++ src/components/Pages/Prompt/Prompt.js
@@ -1,206 +1,205 @@
 import React, { Fragment } from 'react';
-import './Prompt.css';
 
 const Prompt = () => {
-	return (
-		<Fragment>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-			<div className="prompt">
-				<div className="category">Cateogry</div>
-				<div className="author">Author</div>
-				<div className="date">1/1/20 1201am</div>
-				<div className="like">100 Like</div>
-				<div className="unlike">25 Unlike</div>
-				<div className="prompt-content">
-					<h5>Prompt Title</h5>
-					<p>
-						lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf dkfjkdfjkasjfkd
-						fjkadsfjdfkjdskfjdsk fjdkfjdf
-					</p>
-				</div>
-				<div className="comments">10K Comments</div>
-			</div>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+      <div className='prompt'>
+        <div className='category'>Cateogry</div>
+        <div className='author'>Author</div>
+        <div className='date'>1/1/20 1201am</div>
+        <div className='like'>100 Like</div>
+        <div className='unlike'>25 Unlike</div>
+        <div className='prompt-content'>
+          <h5>Prompt Title</h5>
+          <p>
+            lorem dkfjksd fsdkfjkdsfjkdjf kdj fkdsjfkdsjf kdsjfkdsjfdkfjkdsjf
+            dkfjkdfjkasjfkd fjkadsfjdfkjdskfjdsk fjdkfjdf
+          </p>
+        </div>
+        <div className='comments'>10K Comments</div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Prompt;
--- src/components/Pages/Prompts/Prompts.js
+++ src/components/Pages/Prompts/Prompts.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import './Prompts.css';
+import promptheader from '../../../assets/img/promptheader.jpg';
 import Prompt from '../Prompt';
 
 const Prompts = (props) => {
-	console.log(props.match.url + '/test');
-	return (
-		<div className="prompt-wrapper">
-			<div className="prompt-header">
-				<h1>Writing Prompts</h1>
-				<p>Get some inspiration below by thousands of prompts published by authors around the world.</p>
-			</div>
-			<Prompt />
-		</div>
-	);
+  console.log(props.match.url + '/test');
+  return (
+    <div className='prompt-wrapper'>
+      <div className='prompt-header'>
+        <img src={promptheader} alt='writing on paper' />
+        <h1>Writing Prompts</h1>
+        <span>1,502,503 prompts submitted</span>
+        <p>
+          Wheter your story has yet to begin or you're already suffering from
+          witer's block, writing prompts will jump start your brain better than
+          a cup of coffee! Stay for the endless possibilites you and others can
+          imagine with just a little headstart!
+        </p>
+      </div>
+      <Prompt />
+    </div>
+  );
 };
 
 export default Prompts;
