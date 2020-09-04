b1f74ab1c4a249164060e4dc0e90631a08df5a15
Michael Schmidgall <schmidgallm@outlook.com>
2020-09-03 20:53:49 -0700

    register component redirect on success, perttier changed to json, prompt page ui revised

--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,125 +1,123 @@
-import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { setAlert } from '../../../actions/alert';
-import { register } from '../../../actions/auth';
+import React, { useState } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { setAlert } from '../../../actions/alert'
+import { register } from '../../../actions/auth'
 
 const Register = ({ setAlert, register, isAuthenticated }) => {
-  // init state
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: '',
-  });
+    // init state
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
 
-  const { name, email, password, password2 } = formData;
+    const { name, email, password, password2 } = formData
 
-  // on change handler
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    // on change handler
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  // on submit handler
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (password !== password2) {
-      // REDUX ACTION HERE
-      return setAlert('passwords do not match', 'danger');
+    // on submit handler
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if (password !== password2) {
+            // REDUX ACTION HERE
+            return setAlert('passwords do not match', 'danger')
+        }
+        // REDUX ACTION HERE
+        register({ name, email, password })
     }
-    // REDUX ACTION HERE
-    register({ name, email, password });
 
     // Redirect on successful registeration
     if (isAuthenticated) {
-      return <Redirect to='/dashboard' />;
+        return <Redirect to="/dashboard" />
     }
-  };
 
- 
+    return (
+        <div className="container register">
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Full Name</label>
+                    <input
+                        type="name"
+                        className="form-control"
+                        id="name"
+                        name="name"
+                        value={name}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        aria-describedby="emailHelp"
+                        name="email"
+                        value={email}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                    <small id="emailHelp" className="form-text text-muted">
+                        We'll never share your email with anyone else.
+                    </small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password2">Re-Enter Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password2"
+                        name="password2"
+                        value={password2}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
 
-  return (
-    <div className='container register'>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='name'>Full Name</label>
-          <input
-            type='name'
-            className='form-control'
-            id='name'
-            name='name'
-            value={name}
-            onChange={(e) => onChange(e)}
-            required
-          />
+                <button type="submit" className="register-btn btn btn-primary">
+                    Submit
+                </button>
+            </form>
+            <p>
+                Alredy have an account?{' '}
+                <Link to="/login" className="sign-in-msg">
+                    Sign In
+                </Link>
+            </p>
         </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email address</label>
-          <input
-            type='email'
-            className='form-control'
-            id='email'
-            aria-describedby='emailHelp'
-            name='email'
-            value={email}
-            onChange={(e) => onChange(e)}
-            required
-          />
-          <small id='emailHelp' className='form-text text-muted'>
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            className='form-control'
-            id='password'
-            name='password'
-            value={password}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password2'>Re-Enter Password</label>
-          <input
-            type='password'
-            className='form-control'
-            id='password2'
-            name='password2'
-            value={password2}
-            onChange={(e) => onChange(e)}
-            required
-          />
-        </div>
-
-        <button type='submit' className='register-btn btn btn-primary'>
-          Submit
-        </button>
-      </form>
-      <p>
-        Alredy have an account?{' '}
-        <Link to='/login' className='sign-in-msg'>
-          Sign In
-        </Link>
-      </p>
-    </div>
-  );
-};
+    )
+}
 
 // init prop types
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool,
-};
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+}
 
 // map state to props to find if authenticated
 const mapStateToProps = (state) => ({
-  isAuthenticated: state.auth.isAuthenticated,
-});
+    isAuthenticated: state.auth.isAuthenticated,
+})
 
 // connect takes two args. First is any state to map. Second is object with any actions to bring in
 // connect also allows us to use actions as props
-export default connect(mapStateToProps, { setAlert, register })(Register);
+export default connect(mapStateToProps, { setAlert, register })(Register)
--- src/components/Pages/Prompts/Prompts.js
+++ src/components/Pages/Prompts/Prompts.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import promptheader from '../../../assets/img/promptheader.jpg';
-import Prompt from '../Prompt';
+import React from 'react'
+import promptheader from '../../../assets/img/promptheader.jpg'
 
 const Prompts = (props) => {
-  console.log(props.match.url + '/test');
-  return (
-    <div className='prompts-wrapper'>
-      <div className='prompts-header'>
-        <img src={promptheader} alt='writing on paper' />
-        <h1>Writing Prompts</h1>
-        <span>1,502,503 prompts submitted</span>
-        <p>
-          Wheter your story has yet to begin or you're already suffering from
-          witer's block, writing prompts will jump start your brain better than
-          a cup of coffee! Stay for the endless possibilites you and others can
-          imagine with just a little headstart!
-        </p>
-      </div>
-      <Prompt />
-    </div>
-  );
-};
+    console.log(props.match.url + '/test')
+    return (
+        <div className="prompts-wrapper">
+            <div className="prompts-header">
+                <div className="header-hero">
+                    <img src={promptheader} alt="writing on paper" />
+                    <h1>Writing Prompts</h1>
+                </div>
+                <span>1,502,503 prompts submitted</span>
+                <p>
+                    Wheter your story has yet to begin or you're already
+                    suffering from witer's block, writing prompts will jump
+                    start your brain better than a cup of coffee! Stay for the
+                    endless possibilites you and others can imagine with just a
+                    little headstart!
+                </p>
+            </div>
+        </div>
+    )
+}
 
-export default Prompts;
+export default Prompts
+
+/*
+<div className="prompt-container container">
+                <div className="prompt">
+                    <div className="prompt-header">
+                        <span>Rockey Meyers | 8-9-20</span>
+
+                        <span>
+                            {' '}
+                            <i className="fa fa-ellipsis-h"></i>
+                        </span>
+                    </div>
+                    <div className="prompt-body">
+                        <h5>
+                            Prompt Title |{' '}
+                            <span className="text-muted">Science Fiction</span>
+                        </h5>
+                        <p className="prompt-content">
+                            "Sed ut perspiciatis unde omnis iste natus error sit
+                            voluptatem accusantium doloremque laudantium, totam
+                            rem aperiam, eaque ipsa quae ab illo inventore
+                            veritatis et quasi architecto beatae vitae dicta
+                            sunt explicabo. Nemo enim ipsam voluptatem quia
+                            voluptas sit aspernatur aut odit aut fugit, sed quia
+                            consequuntur magni dolores eos qui ratione
+                            voluptatem sequi nesciunt. Neque porro quisquam est,
+                            qui dolorem ipsum quia dolor sit amet, consectetur,
+                            adipisci velit, sed quia non numquam eius modi
+                            tempora incidunt ut labore et dolore magnam aliquam
+                            quaerat voluptatem. Ut enim ad minima veniam, quis
+                            nostrum exercitationem ullam corporis suscipit
+                            laboriosam, nisi ut aliquid ex ea commodi
+                            consequatur? Quis autem vel eum iure reprehenderit
+                            qui in ea voluptate velit esse quam nihil molestiae
+                            consequatur, vel illum qui dolorem eum fugiat quo
+                            voluptas nulla pariatur?"
+                        </p>
+                    </div>
+                    <div className="prompt-footer">
+                        <div>
+                            <span>
+                                <i className="fa fa-thumbs-up"></i>
+                                <span className="mx-2">50K</span>
+                                <i className="fa fa-thumbs-down"></i>
+                            </span>
+                        </div>
+                        <div>
+                            <span>
+                                <i className="fa fa-comment"></i> 659
+                            </span>
+                        </div>
+                        <div>
+                            <span>
+                                <i className="fa fa-heart"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+*/
