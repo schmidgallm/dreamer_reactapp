a23d9d65eefd75a2cfdbcb771632725d2410177b
Michael Schmidgall <schmidgallm@outlook.com>
2020-09-05 18:11:11 -0700

    ui overhaul

--- src/App.js
+++ src/App.js
@@ -1,62 +1,70 @@
 // Dependencies
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 // Redux actions and store
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+import { Provider } from 'react-redux'
+import store from './store'
+import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
 
 // Components
-import Navbar from './components/Layout/Navbar';
-import Landing from './components/Pages/Landing';
-import Register from './components/Auth/Register';
-import Login from './components/Auth/Login';
-import Dashboard from './components/Pages/Dashboard';
-import PrivateRoute from './components/Routing/PrivateRoute';
-import Prompts from './components/Pages/Prompts';
-import Alert from './components/Alert';
-import Footer from './components/Layout/Footer';
-import CreateProfile from './components/Auth/CreateProfile';
-import EditProfile from './components/Auth/EditProfile';
+import Navbar from './components/Layout/Navbar'
+import Home from './components/Pages/Home'
+import Register from './components/Auth/Register'
+import Login from './components/Auth/Login'
+import Dashboard from './components/Pages/Dashboard'
+import PrivateRoute from './components/Routing/PrivateRoute'
+import Prompts from './components/Pages/Prompts'
+import Alert from './components/Alert'
+import Footer from './components/Layout/Footer'
+import CreateProfile from './components/Auth/CreateProfile'
+import EditProfile from './components/Auth/EditProfile'
 
 // CSS
-import './App.css';
+import './App.css'
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+    setAuthToken(localStorage.token)
 }
 
 const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+    useEffect(() => {
+        store.dispatch(loadUser())
+    }, [])
 
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <Alert />
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/prompts' component={Prompts} />
-            <PrivateRoute exact path='/dashboard' component={Dashboard} />
-            <PrivateRoute
-              exact
-              path='/create-profile'
-              component={CreateProfile}
-            />
-            <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-          </Switch>
-          <Footer />
-        </Fragment>
-      </Router>
-    </Provider>
-  );
-};
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path="/" component={Home} />
+                    <Alert />
+                    <Switch>
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/prompts" component={Prompts} />
+                        <PrivateRoute
+                            exact
+                            path="/dashboard"
+                            component={Dashboard}
+                        />
+                        <PrivateRoute
+                            exact
+                            path="/create-profile"
+                            component={CreateProfile}
+                        />
+                        <PrivateRoute
+                            exact
+                            path="/edit-profile"
+                            component={EditProfile}
+                        />
+                    </Switch>
+                    <Footer />
+                </Fragment>
+            </Router>
+        </Provider>
+    )
+}
 
-export default App;
+export default App
