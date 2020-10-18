caa38da2b7536e4e443ecbe0d51cf53f2f821227
Michael Schmidgall <schmidgallm@outlook.com>
2020-10-18 00:01:55 -0700

    add getAllProfiles redux action and reducer

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,11 +1,59 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import {
+    CLEAR_PROFILE,
+    GET_ALL_PROFILES,
+    GET_PROFILE,
+    PROFILE_ERROR,
+} from './types';
 
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('http://localhost:5000/api/v1/profiles/me');
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+// Get all profiles
+export const getAllProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+    try {
+        const res = await axios.get('http://localhost:5000/api/v1/profiles');
+
+        dispatch({
+            type: GET_ALL_PROFILES,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
+    }
+};
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(
+            `http://localhost:5000/api/v1/profiles/users/${userId}`
+        );
+
         dispatch({
             type: GET_PROFILE,
             payload: res.data,
--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -1,38 +1,49 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import {
+    GET_PROFILE,
+    PROFILE_ERROR,
+    CLEAR_PROFILE,
+    GET_ALL_PROFILES,
+} from '../actions/types';
 
 // init initial state
 const initialState = {
-  profile: null,
-  prompts: [],
-  stories: [],
-  loading: true,
-  error: {},
+    profile: null,
+    prompts: [],
+    stories: [],
+    loading: true,
+    error: {},
 };
 
-export default function (state = initialState, action) {
-  const { type, payload } = action;
-  switch (type) {
-    case GET_PROFILE:
-      return {
-        ...state,
-        profile: payload,
-        loading: false,
-      };
-    case PROFILE_ERROR:
-      return {
-        ...state,
-        error: payload,
-        loading: false,
-      };
-    case CLEAR_PROFILE:
-      return {
-        ...state,
-        profile: null,
-        prompts: [],
-        stories: [],
-        loading: false,
-      };
-    default:
-      return state;
-  }
+export default function(state = initialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+        case GET_PROFILE:
+            return {
+                ...state,
+                profile: payload,
+                loading: false,
+            };
+        case GET_ALL_PROFILES:
+            return {
+                ...state,
+                profile: payload,
+                loading: false,
+            };
+        case PROFILE_ERROR:
+            return {
+                ...state,
+                error: payload,
+                loading: false,
+            };
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                prompts: [],
+                stories: [],
+                loading: false,
+            };
+        default:
+            return state;
+    }
 }
