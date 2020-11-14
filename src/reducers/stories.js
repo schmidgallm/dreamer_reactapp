3ef663e6675a254232c267ba06eaaa50efdec00e
Michael Schmidgall <schmidgallm@outlook.com>
2020-11-13 17:42:04 -0700

    adding all story redux reducers

--- src/reducers/stories.js
+++ src/reducers/stories.js
@@ -1,9 +1,13 @@
-// Action Types
 import {
     ADD_STORY,
     GET_STORIES,
     GET_STORY,
     STORY_ERROR,
+    UPDATE_LIKES_STORY,
+    UPDATE_ONE_LIKES_STORY,
+    ADD_COMMENT_STORY,
+    REMOVE_COMMENT_STORY,
+    DELETE_STORY,
 } from '../actions/types';
 
 // init initial state
@@ -35,10 +39,48 @@ export default function(state = initialState, action) {
                 stories: [...state.stories, payload],
                 loading: false,
             };
+        case DELETE_STORY: {
+            return {
+                ...state,
+                stories: state.stories.filter(story => story._id !== payload),
+                loading: false,
+            };
+        }
         case STORY_ERROR:
             return {
                 ...state,
                 error: payload,
+                loading: false,
+            };
+        case UPDATE_LIKES_STORY:
+            return {
+                ...state,
+                stories: state.stories.map(story =>
+                    story._id === payload.id
+                        ? { ...story, likes: payload.likes }
+                        : story
+                ),
+                loading: false,
+            };
+        case UPDATE_ONE_LIKES_STORY:
+            return {
+                ...state,
+                story: { ...state.story1, likes: payload.likes },
+                loading: false,
+            };
+        case ADD_COMMENT_STORY:
+            return {
+                ...state,
+                story: { ...state.story, comments: payload },
+                loading: false,
+            };
+        case REMOVE_COMMENT_STORY:
+            return {
+                ...state,
+                story: {
+                    ...state.story,
+                    comments: payload,
+                },
                 loading: false,
             };
         default:
